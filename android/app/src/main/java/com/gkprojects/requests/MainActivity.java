5c0af6a1277c3520a7e8215906e1eded1b697df3
Samandar Turdiev <firsturdiev@gmail.com>
2024-02-08 20:03:10 +0500

    first commit

---
+++ android/app/src/main/java/com/gkprojects/requests/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gkprojects.request;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        edit: './src/edit.html',
+      },
+    },
+    
+  },
+});
